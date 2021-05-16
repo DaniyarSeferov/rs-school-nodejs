10c3ac717031bdfc1dfe962f74587196886463ca
daniyar.seferov <daniyar.seferov@rockdevels.com>
2021-05-16 21:50:45 +0300

    feat: user mock

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,3 +1,7 @@
-const getAll = async () => [];
+const userMock = require('../../mock/user');
+
+const USER_COUNT = 10;
+
+const getAll = async () => new Array(USER_COUNT).fill().map(userMock.generateUser);
 
 module.exports = { getAll };
